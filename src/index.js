d7342cf9e49ae1d796380417070a88de78c22ef1
Megan Comeau <mlcomeau99@gmail.com>
2020-07-15 18:31:23 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+const form = document.querySelector("#create-task-form");
+const userInput = document.querySelector("#new-task-description");
+const ulTag = document.querySelector("#list #tasks");
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener("submit", (e) => {
+    addTask();
+    e.preventDefault();
+  });
+
+  document.addEventListener('click',function(e){
+      removeTask(e);
+  });
 });
+
+
+function addTask() {
+  let task = userInput.value;
+  let liTag = document.createElement('li');
+  liTag.innerHTML = `${task} <button>&#88;</button>`;
+  ulTag.appendChild(liTag);
+}
+
+function removeTask(e) {
+  if (e.target.tagName == "BUTTON") {
+      e.target.parentElement.remove();
+    }
+}
